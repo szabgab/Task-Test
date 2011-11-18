1d9c12484b57b90de6dc87baabc0dc879b0e8729
Gabor Szabo <szabgab@gmail.com>
2011-11-18 16:57:41 +0200

    initial import

---
+++ lib/Task/Test.pm
@@ -0,0 +1,17 @@
+package Task::Test;
+use strict;
+use warnings;
+use 5.008005;
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Tast::Test - an easy way to install lots of test related modules
+
+=cut
+
+
+
+1;
+
