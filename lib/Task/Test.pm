80a2a2ddd8a935985aef9c116843584ff92317d3
Gabor Szabo <gabor@szabgab.com>
2014-08-01 14:39:45 +0300

    added some modules and released 0.03

--- lib/Task/Test.pm
+++ lib/Task/Test.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.008005;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
@@ -13,6 +13,7 @@ Task::Test - an easy way to install lots of modules needed for testing
 
 For explanation on using some of the modules installed by this module check out
 the slides of L<Test Automaion Using Perl|http://edumaven.com/test-automation-using-perl/>.
+For articles and videos, check out L<Testing with Perl|http://perlmaven.com/testing>.
 
 =head1 AUTHOR
 
